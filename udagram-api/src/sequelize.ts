f7e53e6f88f501fe6dc6f0a7427f986330c5642b
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-06-10 12:56:17 +0200

    fix scripts and envs

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,12 +1,13 @@
-import { Sequelize } from "sequelize-typescript";
-import { config } from "./config/config";
+import {Sequelize} from "sequelize-typescript";
+import {config} from "./config/config";
 
 export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
+	username: config.username,
+	password: config.password,
+	database: config.database,
+	host: config.host,
+	port: config.port_db,
 
-  dialect: "postgres",
-  storage: ":memory:",
+	dialect: "postgres",
+	storage: ":memory:",
 });
